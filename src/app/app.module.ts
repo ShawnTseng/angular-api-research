2fc5c38129a732bf2ed709b1a66d31e3905e6c30
shawntseng <shawntseng@digiwin.biz>
2018-11-22 00:16:22 +0800

    測試NgLocaleLocalization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgLocaleLocalization, registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 @NgModule({
   declarations: [
@@ -13,4 +15,12 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    const tmp1 = registerLocaleData(localeFr, 'fr');
+    const fr = new NgLocaleLocalization('fr');
+    const test1 = fr.getPluralCategory(1.5);
+    const test2 = fr.getPluralCategory(2);
+    console.log(test1, test2);
+  }
+}
